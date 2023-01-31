e6e31a6cdc7b021b17739237b2b1ded44812a3b4
yanndepps <yann.deperrot@protonmail.com>
2023-01-31 02:03:59 +0100

    done U8 S02

---
+++ src/run-python/U8/len_function.py
@@ -0,0 +1,22 @@
+# How many people are in our company at different points in time,
+# after adding and removing people
+# The len() function
+# ---
+
+employees = ["Richard Swift", "Marie-Anne Petrie"]
+num_employees = len(employees)
+mess = "At the beginning, there are " + str(num_employees) + " employees"
+print(mess)
+
+employees.append("Cody Lightfoot")
+employees.append("Laure Simmons")
+
+num_employees = len(employees)
+mess = "The company grows and now has " + str(num_employees) + " employees"
+print(mess)
+
+employees.pop(1)
+
+num_employees = len(employees)
+mess = "Marie-Anne left the company. There are now " + str(num_employees) + " employees"
+print(mess)
